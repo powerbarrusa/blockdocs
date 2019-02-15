52e77ff8027b9a1dc3f11d4444b22484000028f0
powerbarrusa <barr@covrigaru.com>
2019-02-15 13:24:29 -0700

    heroku push

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 app.use(express.static('public'))
 
-app.get('/', (req, res, next) => {
+app.get('/docs', (req, res, next) => {
   knex('docs')
   .then((rows) => {
     res.send(rows)
